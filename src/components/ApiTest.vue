226d77965fe0921cd4052818ace117faac0fa7c8
Jason Law <jason.lloyd.law@gmail.com>
2019-11-05 21:37:53 +1000

    spa project dependencies added. ready to build

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <h2 class="text-indigo-600 font-bold text-3xl">{{message}}</h2>
+      <div class="flex justify-center">
+        <button @click="api" 
+        class="px-4 py-2 border border-2 bg-indigo-400 text-xl font-semibold text-indigo-800
+        hover:bg-indigo-300 active:bg-indigo-500">
+            click to test
+        </button>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'api-test',
+    data(){
+        return {
+            message: 'now if i test it again',
+        }
+    },
+    methods:{
+        api(){
+            this.axios.get('http://backend.test/test').then(response =>{
+                this.message = response.data.message;
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// import 'es6-promise/auto';
 import App from './App.vue'
 import store from './store'
 
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
+
+Vue.component('hello-world', require('./components/HelloWorld.vue').default)
+Vue.component('api-test', require('./components/ApiTest.vue').default)
 
 new Vue({
   store,
